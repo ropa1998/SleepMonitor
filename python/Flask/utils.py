dd74ac538d4dd09805c801a5a2fdc5002d9f64a2
rodrigo <ropa1998@gmail.com>
2019-10-18 13:41:44 -0300

    Created a method that converts a string with a specific format to a datetime

---
+++ python/Flask/utils.py
@@ -0,0 +1,13 @@
+import datetime as datetime
+
+
+def parse_time(date_str: str):
+    date_time_obj = datetime.datetime.strptime(date_str, '%Y-%m-%dT%H:%M')
+    return date_time_obj
+
+
+# dt = "2019-10-12T01:00"
+# parse_date = parse_time(dt)
+# print('Date:', parse_date.date())
+# print('Time:', parse_date.time())
+# print('Date-time:', parse_date)

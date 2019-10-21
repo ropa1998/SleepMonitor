c23c588f197033c5eb5b45fc843457a07c1f47b8
rodrigo <ropa1998@gmail.com>
2019-10-21 08:51:04 -0300

    Finished documenting email_sender

--- python/Flask/email_sender.py
+++ python/Flask/email_sender.py
@@ -1,3 +1,7 @@
+## @package email_sender
+#  This module takes care of sending emails.
+
+
 import yagmail
 import keyring
 
@@ -13,7 +17,11 @@ PASSWORD = keyring.get_password("gmail", USERNAME)
 
 yag = yagmail.SMTP(USERNAME, PASSWORD)
 
-
+##  This method sends emails with the paramaters that are passed.
+# @param to The receiver of the email
+# @param subject The subject of the email
+# @param body The body of the email
+# @param attachments The attachments for the emails, passed as a list with their absolute paths.
 def send_email(to: str, subject: str, body: str, attachments: []):
     yag.send(
         to=to,

6fd6560dde298e5c4d119a027ac85294c8f3a68a
rodrigo <ropa1998@gmail.com>
2019-10-23 18:22:34 -0300

    Improved database values manager.

--- python/Flask/serial_reader.py
+++ python/Flask/serial_reader.py
@@ -1,17 +1,21 @@
 ## @package serial_reader
 # This module takes care of reading data from the serial port, formatting it and saving it to the database
 import sqlite3 as lite
-
+from datetime import datetime
+from SleepMonitor.python.Flask.date_parser import STANDARD_FORMAT
 import serial
 
 
 ## This method saves the sensors data into the database
 # @param read_list An array of values containing the temperature, humidity and light values.
+
+
 def save_data(read_list):
     con = lite.connect('../sensorsData.db')
     with con:
         cur = con.cursor()
-        query = "INSERT INTO DHT_data VALUES(datetime('now')," + read_list[0] + "," + read_list[1] + "," + read_list[
+        std_frmt = str(datetime.now().strftime(STANDARD_FORMAT))
+        query = "INSERT INTO DHT_data VALUES(" + std_frmt + "," + read_list[0] + "," + read_list[1] + "," + read_list[
             2] + " )"
         print(query)
         cur.execute(query)
